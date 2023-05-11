3878f8038eb0ec91668558234253320ec7e0c377
Hernan Finucci <hfinucci@itba.edu.ar>
2023-05-11 19:58:35 -0300

    RE-factoring

--- src/main/java/sistem2/GPC5.java
+++ src/main/java/sistem2/GPC5.java
@@ -8,29 +8,32 @@ import java.util.List;
 
 public class GPC5 {
 
-    private static final double[] alpha = {3.0/16, 251.0/360, 1, 11.0/18, 1.0/6, 1.0/60};
-    private static final ArrayList<Ball> balls = new ArrayList<>();
-    private static List<List<Tuple>> Rs = new ArrayList<>();
+    private static final double[] alpha = { 3.0/16, 251.0/360, 1, 11.0/18, 1.0/6, 1.0/60 };
+    private static final List<Ball> balls = new ArrayList<>();
+    private static List<List<Tuple>> Rs;
+    private static final double dT = 200;
 
-    public static List<ArrayList<Double>> gear(double dT, double tf){
+    private static final double tf = 1000;
 
-        List<ArrayList<Double>> states = new ArrayList<>();
+    public static List<List<Double>> gear() {
+
+        List<List<Double>> states = new ArrayList<>();
         double t = dT;
         List<List<Tuple>> currentRs = Rs;
 
-        while(t <= tf) {
-            // Me guardo el estado
+        while (t <= tf) {
             for (Ball ball : balls) {
                 ArrayList<Double> state = new ArrayList<>();
-                ArrayList<Double> position = currentRs.get(ball.getId()).get(0);
-                ArrayList<Double> velocities = currentRs.get(ball.getId()).get(1);
+                Tuple position = currentRs.get(ball.getId()).get(0);
+                Tuple velocity = currentRs.get(ball.getId()).get(1);
 
                 state.add(t);
                 state.add((double) ball.getId());
-                state.add(position.get(0));
-                state.add(position.get(1));
-                state.add(velocities.get(0));
-                state.add(velocities.get(1));
+                state.add(position.getA());
+                state.add(position.getB());
+                state.add(velocity.getA());
+                state.add(velocity.getB());
+
                 states.add(state);
             }
 
@@ -44,24 +47,20 @@ public class GPC5 {
             currentRs = gearCorrector(newDerivatives, dT, deltasR2);
 
             t += dT;
-//            System.out.println("dps de una vuelta" + currentRs);
         }
         return states;
     }
-    public static List<List<Tuple>> initialRs(){
-        List<List<Tuple>> R = new ArrayList<>();
-//        Lista de particulas -> cada una tiene una lista de Rs (que son tuplas)
-//       [Sol: [ [r0x r0y] [r1x r1y] ] [ [r0x r0y] [r1x r1y] ], Tierra: [ [r0x r0y] [r1x r1y] ] [ [r0x r0y] [r1x r1y] ]]
-        Tuple r;
+    public static void initialRs(){
+//      Lista de particulas -> cada una tiene una lista de Rs (que son tuplas)
         for (Ball ball: balls) {
             List<Tuple> auxR = new ArrayList<>();
+
             //r0
             auxR.add(new Tuple(ball.getX(), ball.getY()));
             //r1
-            auxR.add(new Tuple(ball.getVx(), ball.getVy()));
+            auxR.add(new Tuple(ball.getSpeedX(), ball.getSpeedY()));
             //r2
-            Tuple a = calculateA(ball,null);
-            auxR.add(new Tuple(a.getA(), a.getB()));
+            auxR.add(calculateA(ball,null));
             //r3
             auxR.add(new Tuple(0.0, 0.0));
             //r4
@@ -69,9 +68,8 @@ public class GPC5 {
             //r5
             auxR.add(new Tuple(0.0, 0.0));
 
-            R.add(auxR);
+            Rs.add(auxR);
         }
-        return R;
     }
 
     public static List<List<Tuple>> gearPredictor(List<List<Tuple>> der, double dT){
@@ -118,58 +116,46 @@ public class GPC5 {
         return newDerivatives;
     }
 
-    public static List<List<ArrayList<Double>>> gearCorrector(List<List<ArrayList<Double>>> der, double dT, List<ArrayList<Double>>  dR2){
+    public static List<List<Tuple>> gearCorrector(List<List<Tuple>> der, double dT, List<Tuple> dR2){
         // [ [ [r0x r0y] [r1x r1y] ] [ [r2x r2y] [r3x r3y] ] ]
-        List<List<ArrayList<Double>>> newDerivatives = new ArrayList<>();
+        List<List<Tuple>> newDerivatives = new ArrayList<>();
         int count = 0;
-        for(List<ArrayList<Double>> rs : der) { // por planeta
+        for(List<Tuple> rs : der) { // por planeta
 
-            List<ArrayList<Double>> auxNewDerivatives = new ArrayList<>();
+            List<Tuple> auxNewDerivatives = new ArrayList<>();
 
             // dR2 = [ sol: [dr2x dr2y] ,tierra: [dr2x dr2y] ,venus: [dr2x dr2y], nave: [dr2x dr2y] ]
-            double r0x = rs.get(0).get(0) + (alpha[0] * dR2.get(count).get(0));
-            double r0y = rs.get(0).get(1) + (alpha[0] * dR2.get(count).get(1));
-            ArrayList<Double> r0 = new ArrayList<>();
-            r0.add(r0x);
-            r0.add(r0y);
+            double r0x = rs.get(0).getA() + (alpha[0] * dR2.get(count).getA());
+            double r0y = rs.get(0).getB() + (alpha[0] * dR2.get(count).getB());
+            Tuple r0 = new Tuple(r0x, r0y);
             auxNewDerivatives.add(r0);
 
-            double r1x = rs.get(1).get(0) + (alpha[1] * dR2.get(count).get(0) * 1 ) / (dT);
-            double r1y = rs.get(1).get(1) + (alpha[1] * dR2.get(count).get(0) * 1 ) / (dT);
-            ArrayList<Double> r1 = new ArrayList<>();
-            r1.add(r1x);
-            r1.add(r1y);
+            double r1x = rs.get(1).getA() + (alpha[1] * dR2.get(count).getA() * 1 ) / (dT);
+            double r1y = rs.get(1).getB() + (alpha[1] * dR2.get(count).getA() * 1 ) / (dT);
+            Tuple r1 = new Tuple(r1x, r1y);
             auxNewDerivatives.add(r1);
 
 
-            double r2x = rs.get(2).get(0) + (alpha[2] * dR2.get(count).get(0) * 2) / (dT * dT);
-            double r2y = rs.get(2).get(1) + (alpha[2] * dR2.get(count).get(1) * 2) / (dT * dT);
-            ArrayList<Double> r2 = new ArrayList<>();
-            r2.add(r2x);
-            r2.add(r2y);
+            double r2x = rs.get(2).getA() + (alpha[2] * dR2.get(count).getA() * 2) / (dT * dT);
+            double r2y = rs.get(2).getB() + (alpha[2] * dR2.get(count).getB() * 2) / (dT * dT);
+            Tuple r2 = new Tuple(r2x, r2y);
             auxNewDerivatives.add(r2);
 
 
-            double r3x = rs.get(3).get(0) + (alpha[3] * dR2.get(count).get(0) * 6) / (dT * dT * dT);
-            double r3y = rs.get(3).get(1) + (alpha[3] * dR2.get(count).get(1) * 6) / (dT * dT * dT);
-            ArrayList<Double> r3 = new ArrayList<>();
-            r3.add(r3x);
-            r3.add(r3y);
+            double r3x = rs.get(3).getA() + (alpha[3] * dR2.get(count).getA() * 6) / (dT * dT * dT);
+            double r3y = rs.get(3).getB() + (alpha[3] * dR2.get(count).getB() * 6) / (dT * dT * dT);
+            Tuple r3 = new Tuple(r3x, r3y);
             auxNewDerivatives.add(r3);
 
 
-            double r4x = rs.get(4).get(0) + (alpha[4] * dR2.get(count).get(0) * 24) / (dT * dT * dT * dT);
-            double r4y = rs.get(4).get(1) + (alpha[4] * dR2.get(count).get(1) * 24) / (dT * dT * dT * dT);
-            ArrayList<Double> r4 = new ArrayList<>();
-            r4.add(r4x);
-            r4.add(r4y);
+            double r4x = rs.get(4).getA() + (alpha[4] * dR2.get(count).getA() * 24) / (dT * dT * dT * dT);
+            double r4y = rs.get(4).getB() + (alpha[4] * dR2.get(count).getB() * 24) / (dT * dT * dT * dT);
+            Tuple r4 = new Tuple(r4x, r4y);
             auxNewDerivatives.add(r4);
 
-            double r5x = rs.get(5).get(0) + (alpha[5] * dR2.get(count).get(0) * 120) / (dT * dT * dT * dT * dT);
-            double r5y = rs.get(5).get(1) + (alpha[5] * dR2.get(count).get(1) * 120) / (dT * dT * dT * dT * dT);
-            ArrayList<Double> r5 = new ArrayList<>();
-            r5.add(r5x);
-            r5.add(r5y);
+            double r5x = rs.get(5).getA() + (alpha[5] * dR2.get(count).getA() * 120) / (dT * dT * dT * dT * dT);
+            double r5y = rs.get(5).getB() + (alpha[5] * dR2.get(count).getB() * 120) / (dT * dT * dT * dT * dT);
+            Tuple r5 = new Tuple(r5x, r5y);
             auxNewDerivatives.add(r5);
 
             count++;
@@ -178,28 +164,25 @@ public class GPC5 {
         return newDerivatives;
     }
 
-    private static Tuple calculateA(Ball ball, List<List<ArrayList<Double>>> Rs) {
+    private static Tuple calculateA(Ball ball, List<List<Tuple>> Rs) {
 
         // TODO calcular la fueraza de cada particula
 
         return new Tuple(0.0, 0.0);
     }
 
-    private static List<ArrayList<Double>> getR2(List<List<ArrayList<Double>>> newDerivatives, double dT){
-        List<ArrayList<Double>> deltasR2 = new ArrayList<>();
+    private static List<Tuple> getR2(List<List<Tuple>> newDerivatives, double dT){
+        List<Tuple> deltasR2 = new ArrayList<>();
         for (Ball ball : balls) {
 
             Tuple A = calculateA(ball, newDerivatives);
             // TODO a checkear
-            ArrayList<Double> r2 = newDerivatives.get(ball.getId()).get(2);
+            Tuple r2 = newDerivatives.get(ball.getId()).get(2);
 
-            double dR2X = (A.getA() - r2.get(0)) * dT*dT / 2;
-            double dR2Y = (A.getB() - r2.get(1)) * dT*dT / 2;
+            double dR2X = (A.getA() - r2.getA()) * dT*dT / 2;
+            double dR2Y = (A.getB() - r2.getB()) * dT*dT / 2;
 
-            ArrayList<Double> deltaR2 = new ArrayList<>();
-            deltaR2.add(dR2X);
-            deltaR2.add(dR2Y);
-            deltasR2.add(deltaR2);
+            deltasR2.add(new Tuple(dR2X, dR2Y));
         }
         return deltasR2;
         // [ sol: [dr2x dr2y] ,tierra: [dr2x dr2y] ,venus: [dr2x dr2y], nave: [dr2x dr2y] ]
--- src/main/java/sistem2/PoolSimulation.java
+++ src/main/java/sistem2/PoolSimulation.java
@@ -1,7 +1,39 @@
 package sistem2;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static sistem2.GPC5.gear;
+import static sistem2.GPC5.initialRs;
+
 public class PoolSimulation {
     public static void main(String[] args) {
 
+        initialRs();
+
+        List<List<Double>> states = gear();
+        generateOutput(states);
+    }
+
+    private static void generateOutput(List<List<Double>> states) {
+        try {
+            FileWriter myWriter = new FileWriter("src/resources/states1_1.txt");
+            int count = 0;
+            for (List<Double> frame : states) {
+                if(count == 4|| count == 0){
+                    myWriter.write(frame.get(0) + "\n");
+                    count = 0;
+                }
+                myWriter.write(frame.get(1) + "\t" + frame.get(2) + "\t" + frame.get(3) + "\t" + frame.get(4) + "\t" + frame.get(5) + "\n");
+                count++;
+            }
+            myWriter.close();
+            System.out.println("Successfully wrote states1_1.txt.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
 }
